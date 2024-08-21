4e9c9e86ca6e72314342842d8b09ed3dbbc3eea4
Benjamin Ridges <brid0004@student.monash.edu>
2024-08-21 15:26:31 +1000

    Include PrimeVue and Bootstrap

--- FreshMind/src/main.js
+++ FreshMind/src/main.js
@@ -1,11 +1,19 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes'
 
+const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 app.use(router)
 
 app.mount('#app')
