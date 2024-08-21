573dd66da56e254c385f7fb4456f3319d021650c
Benjamin Ridges <brid0004@student.monash.edu>
2024-08-21 15:28:36 +1000

    fix aura theme import

--- FreshMind/src/main.js
+++ FreshMind/src/main.js
@@ -6,14 +6,14 @@ import App from './App.vue'
 import router from './router'
 
 import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes'
+import Aura from '@primevue/themes/aura'
 
 const app = createApp(App)
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
+  theme: {
+    preset: Aura
+  }
+})
 app.use(router)
 
 app.mount('#app')
