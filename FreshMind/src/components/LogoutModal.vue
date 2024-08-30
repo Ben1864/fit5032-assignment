ac0a8b1713003a3888f24c5a27023822665e4898
Benjamin Ridges <brid0004@student.monash.edu>
2024-08-30 15:03:04 +1000

    Logout Modal

---
+++ FreshMind/src/components/LogoutModal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Are you sure you want to Logout?</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-footer justify-content-center">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary red-btn " @click="logout()" data-bs-dismiss="modal">Logout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+
+<script setup>
+import store from '@/store/store';
+const logout = () => {
+    try{
+        store.dispatch('logout');
+    }catch (error) {
+        console.error('Error logging out:', error);
+    }
+}
+</script>
+  <style>
+  .red-btn{
+      --bs-btn-bg: red !important;
+      --bs-btn-hover-bg: #db0707 !important;
+      --bs-btn-active-bg: #bd0606 !important;
+  }
+  </style>
