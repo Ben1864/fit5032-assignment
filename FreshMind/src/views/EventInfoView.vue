275524f6cf4c3cd526519847cbf235ee705b4630
Benjamin Ridges <brid0004@student.monash.edu>
2024-09-01 00:31:17 +1000

    display event information

--- FreshMind/src/views/EventInfoView.vue
+++ FreshMind/src/views/EventInfoView.vue
@@ -4,14 +4,32 @@
             <div class="col-lg-6 col-12 d-flex pe-3 item-container align-items-center">
                 <img :src="currentEvent.image" class="img-fluid fill-area"></img>
             </div>
-            <div class="col-6">
+            <div class="col-lg-6 col-12">
                 <div class="row">
                     <h1>{{ currentEvent.title }}</h1>
                 </div>
                 <div class="row">
-                    <div class="col-1">
-                        <span class="badge badge-not-attending">{{currentEvent.status}}</span>
+                    <div class="col-12 d-flex align-items-center">
+                        <span class="badge badge-not-attending me-2">{{ currentEvent.status }}</span>
+                        <span v-for="tag in currentEvent.tags" class="badge badge-tags me-2">{{ tag }}</span>
                     </div>
+                </div>
+                <div class="row mt-3">
+                    <h2>Location</h2><br>
+                    <div class="event-info">{{ currentEvent.location }}</div>
+                </div>
+                <div class="row mt-3">
+                    <h2>Time</h2><br>
+                    <div class="event-info">{{ currentEvent.startTime }} - {{ currentEvent.finishTime }}, {{ currentEventDate }}</div>
+                </div>
+                <div class="row mt-3">
+                    <h2>Description</h2><br>
+                    <div class="event-info">{{ currentEvent.description }}</div>
+                </div>
+                <div class="row mt-3">
+                    <h2>Contact Details</h2><br>
+                    <div class="event-info">{{ currentEvent.organiser }}, {{ currentEvent.contact.email }}</div><br>
+                    <div class="event-info">{{ currentEvent.contact.phone }}</div>
                 </div>
             </div>
         </div>
@@ -33,6 +51,17 @@
         currentEvent.status = 'Not Attending'
     }
    
+    const convertDateFormat = (dateStr) => {
+        const date = new Date(dateStr);
+
+        return new Intl.DateTimeFormat('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }).format(date);
+    }
+    const currentEventDate = convertDateFormat(currentEvent.date);
 
 </script>
 
@@ -52,9 +81,18 @@
     background-color: #dc3545;
 }
 
+.badge-tags {
+    background-color: #6c757d;
+}
+
 .fill-area {
     width: 100%;
     object-fit: cover;
     border: 2px black solid;
+}
+
+.event-info{
+    font-size: larger;
+    padding-left: 40px;
 }
 </style>
