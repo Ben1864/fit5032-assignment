9782e07e855f20ee5159033678f729b035a573fc
Benjamin Ridges <brid0004@student.monash.edu>
2024-08-31 12:36:37 +1000

    find event by id

---
+++ FreshMind/src/views/EventInfoView.vue
@@ -0,0 +1,9 @@
+<template>
+
+</template>
+
+<script setup>
+    import events from '../assets/json/events.json'
+    const props = defineProps({ id: String })
+    const currentEvent = events.find(event => props.id == event.id)
+</script>
