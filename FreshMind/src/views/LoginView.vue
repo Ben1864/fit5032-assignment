9999746355c7734d228358eb546111aabaf5b527
Benjamin Ridges <brid0004@student.monash.edu>
2024-10-09 17:49:54 +1100

    Delete sign in with google

--- FreshMind/src/views/LoginView.vue
+++ FreshMind/src/views/LoginView.vue
@@ -5,20 +5,16 @@ import LoginForm from '@/components/LoginForm.vue';
 
 <template>
     <div class="row justify-content-center align-items-center" style="height: 80vh;">
-        <div class="col-lg-6 col-12 border-end pe-3">
+        <div class="col-12 pe-3">
             <h1 class="text-center">Login with Email</h1>
             <LoginForm></LoginForm>
         </div>
-        <div class="col-6">
-            <h1 class="text-center">Sign in with Google</h1>
-            <div class="row">
-                <div class="text-center vertical-divider">or</div>
-            </div>
-            <div class="row mb-3 text-center justify-content-center">
+        <div class="text-center">
+            <p style="font-size: 20px">or</p>
+            <div class="row mb-3 justify-content-center">
                 <router-link to="/signup">
                     <button type="button" class="btn btn-secondary signup-btn">Sign Up</button>
                 </router-link>
-                
             </div>
         </div>
     </div>
@@ -37,9 +33,17 @@ import LoginForm from '@/components/LoginForm.vue';
 .signup-btn{
     background-color: #8E4739;
     font-size: 32px;
-    width: 80%;
+    width: 50%;
     --bs-btn-hover-bg: #8a5e55;
     --bs-btn-active-bg: #4f2e27;
+}
+
+.google-btn{
+    background-color: #a86c60;
+    font-size: 32px;
+    width: 60%;
+    --bs-btn-hover-bg: #b3837a;
+    --bs-btn-active-bg: #654943;
 }
 
 .vertical-divider{
