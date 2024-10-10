6724335f2bc53a13c3188e5a5745da567ab48aa8
Benjamin Ridges <brid0004@student.monash.edu>
2024-10-11 10:22:53 +1100

    Exports as PDF

--- FreshMind/functions/index.js
+++ FreshMind/functions/index.js
@@ -8,7 +8,34 @@
  */
 
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const cors = require("cors")({origin: true});
+const postmark = require('postmark');
+
+const client = new postmark.ServerClient("REDCATED_KEY");
+
+exports.sendEmail = onRequest(async (req, res) =>{
+  cors(req, res, async () => {
+    const {from, subject, message} = req.body;
+    if (!from || !subject || !message) {
+      return res.status(400).send("Missing required fields.");
+    }
+    try {
+      const response = await client.sendEmail({
+        From: from,
+        To: "brid0004@student.monash.edu",
+        Subject: subject,
+        TextBody: message,
+      });
+      console.log("Email sent successfully: ", response);
+      return res.status(200).send("Email sent successfully");
+    } catch(error) {
+      console.error("Error sending email: ", error);
+      return res.status(500).send("Error sending email")
+    }
+  });
+})
+
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
