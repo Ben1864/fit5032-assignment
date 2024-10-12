324e9879ee4d3e472ce8e4b6a1a6ed29889b6e25
Benjamin Ridges <brid0004@student.monash.edu>
2024-10-12 17:19:20 +1100

    Remove unused headers

--- FreshMind/src/router/index.js
+++ FreshMind/src/router/index.js
@@ -12,14 +12,6 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/login',
       name: 'Login',
       component: () => import('../views/LoginView.vue'),
